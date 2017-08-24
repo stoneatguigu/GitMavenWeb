0ba27a4f62e1dc368e09b94f539b427dbc022543
lhuc2017 <lhuc2017@126.com>
2017-08-24 20:48:30 +0800

    dugu

--- src/main/java/com/atguigu/git/test/Apple.java
+++ src/main/java/com/atguigu/git/test/Apple.java
@@ -8,6 +8,7 @@ public class Apple {
 		System.out.println("2222222222");
 		System.out.println("111111111111");
 		System.out.println("33333");
+		System.out.println("dugujiujian is very fast");
 	}
 
 }
