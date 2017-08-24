9bcc60a52d6c9ce5c235dca8077028df5da5454d
lhuc2017 <lhuc2017@126.com>
2017-08-24 20:18:06 +0800

    submit by lin

--- src/main/java/com/atguigu/git/test/Apple.java
+++ src/main/java/com/atguigu/git/test/Apple.java
@@ -3,6 +3,7 @@ package com.atguigu.git.test;
 public class Apple {
 	public static void main(String[] args) {
 		System.out.println("edit by lhuc ...");
+		System.out.println("edit by lhuc ...");
 	}
 
 }
